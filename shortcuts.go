1bf8d5a008137be14105344c9d46c30f2ab682a0
codemodify <codemodify@member.fsf.org>
2020-08-22 15:19:40 -0700

    save

--- shortcuts.go
+++ shortcuts.go
@@ -1,28 +1,271 @@
 package os
 
-import (
-	"fmt"
-	"strings"
-)
-
-// IsDarwin -
-func IsDarwin(name Name) bool {
-	return (name == OS_Darwin || name == OS_MacOSX)
-}
-
-// IsBSD -
-func IsBSD(name Name) bool {
-	if strings.Contains(strings.ToLower(fmt.Sprint("", name)), "bsd") {
-		return true
+func IsFamilyAIX(name Name) bool {
+	for _, osName := range osNameFamilyAIX {
+		if name == osName {
+			return true
+		}
 	}
 
-	return (name == OS_DragonflyBSD || name == OS_Bitrig)
+	return false
 }
 
-// IsWindows -
-func IsWindows(name Name) bool {
-	return (name == OS_Windows ||
-		name == OS_CYGWIN ||
-		name == OS_MSYS ||
-		name == OS_MINGW)
+func IsFamilyDarwin(name Name) bool {
+	for _, osName := range osNameFamilyDarwin {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyDragonflyBSD(name Name) bool {
+	for _, osName := range osNameFamilyDragonflyBSD {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyFreeBSD(name Name) bool {
+	for _, osName := range osNameFamilyFreeBSD {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyHURD(name Name) bool {
+	for _, osName := range osNameFamilyHURD {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyIllumos(name Name) bool {
+	for _, osName := range osNameFamilyIllumos {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyJS(name Name) bool {
+	for _, osName := range osNameFamilyJS {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxAndroid(name Name) bool {
+	for _, osName := range osNameFamilyLinuxAndroid {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxArch(name Name) bool {
+	for _, osName := range osNameFamilyLinuxArch {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxCentos(name Name) bool {
+	for _, osName := range osNameFamilyLinuxCentos {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxDebian(name Name) bool {
+	for _, osName := range osNameFamilyLinuxDebian {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxFedora(name Name) bool {
+	for _, osName := range osNameFamilyLinuxFedora {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxGentoo(name Name) bool {
+	for _, osName := range osNameFamilyLinuxGentoo {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxKnoppix(name Name) bool {
+	for _, osName := range osNameFamilyLinuxKnoppix {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxSlackware(name Name) bool {
+	for _, osName := range osNameFamilyLinuxSlackware {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxSlax(name Name) bool {
+	for _, osName := range osNameFamilyLinuxSlax {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxSuse(name Name) bool {
+	for _, osName := range osNameFamilyLinuxSuse {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinuxUbuntu(name Name) bool {
+	for _, osName := range osNameFamilyLinuxUbuntu {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyLinux(name Name) bool {
+	for _, osName := range osNameFamilyLinux {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyNACL(name Name) bool {
+	for _, osName := range osNameFamilyNACL {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyNetBSD(name Name) bool {
+	for _, osName := range osNameFamilyNetBSD {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyOpenBSD(name Name) bool {
+	for _, osName := range osNameFamilyOpenBSD {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyOther(name Name) bool {
+	for _, osName := range osNameFamilyOther {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyPlan9(name Name) bool {
+	for _, osName := range osNameFamilyPlan9 {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilySolaris(name Name) bool {
+	for _, osName := range osNameFamilySolaris {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyWindows(name Name) bool {
+	for _, osName := range osNameFamilyWindows {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsFamilyZOS(name Name) bool {
+	for _, osName := range osNameFamilyZOS {
+		if name == osName {
+			return true
+		}
+	}
+
+	return false
 }
